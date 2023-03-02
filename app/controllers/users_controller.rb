f93040403b0a16145e9c7930f289a965ba7f9227
Muriithi <muriithi.kibata@student.moringaschool.com>
2023-03-02 22:44:45 +0300

    Done

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,12 @@
 class UsersController < ApplicationController
+    rescue_from ActiveRecord::RecordInvalid, with: :invalid_response
     skip_before_action :authorize, only: [:create]
     def index
         render json: User.all
     end
 
     def create
-        user  = User.create(user_params)
+        user  = User.create!(user_params)
         token = encode_token({user_id: user.id})
         render json: [UserSerializer.new(user), {token: token}], status: :created
     end
@@ -15,4 +16,9 @@ class UsersController < ApplicationController
     def user_params
         params.permit(:email, :password, :password_confirmation)
     end
+
+    def invalid_response(invalid)
+        render json: {errors: invalid.record.errors.full_messages}, status: :unprocessable_entity
+    end
+
 end
